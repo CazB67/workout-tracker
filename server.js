c4e9a35a525fb48cff6876eec132ac3f0de8f82f
Caroline <caroline.portilla@gmail.com>
2020-06-14 09:57:38 +0800

    html routes

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-const express = require("express");
+let express = require("express");
+let app = express();
+const PORT = process.env.PORT || 3000;
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-
 //const db = require("./models");
 
-const app = express();
-
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
-
+require("./routes/html-routes.js")(app);
+//require("./routes/api-routes.js")(app);
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workoutTrackerDB";
-mongoose.connect(MONGODB_URI), {useNewUrlParser: true };
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true });
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
