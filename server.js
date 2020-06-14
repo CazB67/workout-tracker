9f985633bb2852a7cc05fd112ccdc8853b8e2d83
Caroline <caroline.portilla@gmail.com>
2020-06-14 20:58:40 +0800

    moongoose models created

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ let app = express();
 const PORT = process.env.PORT || 3000;
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
 const db = require("./models");
 
 app.use(logger("dev"));
@@ -12,8 +11,8 @@ app.use(express.json());
 app.use(express.static("public"));
 require("./routes/html-routes.js")(app);
 //require("./routes/api-routes.js")(app);
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workoutTrackerDB";
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true });
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useFindAndModify: false });
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
